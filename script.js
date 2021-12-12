917ca741eb015f0581a56e3ed9e8e830a7db0968
jessejputnam <jessejputnam@gmail.com>
2021-12-12 00:48:45 -0500

    Add game functionality

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ const playPaper = document.querySelector(".player__playarea__paper");
 const playScissors = document.querySelector(".player__playarea__scissors");
 const actionPlayRound = document.querySelector(".player__action__btn");
 const computerChoiceImg = document.querySelector(".comp__playarea__turn");
-const computerChoiceDisplay = document.querySelector(
-  ".comp__playarea__compchoice"
-);
 const displayPlayerScore = document.querySelector(".scoreboard__player__score");
 const displayComputerScore = document.querySelector(".scoreboard__comp__score");
 const gameOver = document.querySelector(".gameover__title");
+const playerNoSelect = document.querySelector(".player__no-selection");
+const nextRoundBtn = document.querySelector(".next__round");
+const nextRoundBtnBold = document.querySelector(".next__round--bold");
+const startGame = document.querySelector(".modal");
+const startGameTitle = document.querySelector(".modal-title");
+const startGameText = document.querySelector(".modal-p");
+const mainPage = document.querySelector(".main-page");
+const gameBoard = document.querySelector(".gameboard");
 
 // DECLARE STORED INFO VARIABLES
-let playerSelect, computerSelect;
+let playerSelect, computerSelect, roundWinner, gameWinner;
 let computerScoreStored = 0;
 let playerScoreStored = 0;
 
@@ -23,17 +28,28 @@ const computerPlay = function () {
   let roll = Math.ceil(Math.random() * 3);
   return roll === 1 ? "rock" : roll === 2 ? "paper" : "scissors";
 };
-const newRound = function () {
-  playerSelect = "";
+
+// GAME STATES
+const nextRound = function () {
+  roundWinner = "";
+  playerSelect = undefined;
   computerSelect = "";
+
+  nextRoundBtn.classList.add("hidden");
+
+  computerChoiceImg.src = "images/question-mark.png";
+  playRock.classList.remove("selected-item");
+  playPaper.classList.remove("selected-item");
+  playScissors.classList.remove("selected-item");
 };
 
 // Player select choice
 playerSelectChoice.forEach((selection) => {
   selection.addEventListener("click", (e) => {
+    if (mainPage.classList.contains("background--blur")) return;
     playerSelect = selection.alt;
-    // add in CSS class AND transition height and width 1s in player__choice style block
     selection.classList.add("selected-item");
+    if (playerSelect !== undefined) playerNoSelect.classList.add("hidden");
   });
 });
 
@@ -44,83 +60,114 @@ document.addEventListener("click", () => {
     playScissors.classList.remove("selected-item");
 });
 
-// Play Round
-actionPlayRound.addEventListener("click", () => {
-  computerSelect = computerPlay();
-  computerChoiceDisplay.innerText = computerSelect;
-});
+// Start Game
+startGame.addEventListener("click", () => {
+  gameBoard.classList.remove("background--blur");
+  mainPage.classList.remove("background--blur");
+  startGame.classList.add("hidden");
 
-/*
-// Computer input
-const computerPlay = function () {
-  let roll = Math.ceil(Math.random() * 3);
-  return roll === 1 ? "rock" : roll === 2 ? "paper" : "scissors";
-};
+  playerScoreStored = 0;
+  computerScoreStored = 0;
+  displayPlayerScore.innerText = playerScoreStored;
+  displayComputerScore.innerText = computerScoreStored;
 
-// Play a round
-const playRound = function (playerSelection, computerSelection) {
-  // player lose
-  if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
-  ) {
-    return "Lose";
-  }
+  playRock.classList.remove("selected-item");
+  playPaper.classList.remove("selected-item");
+  playScissors.classList.remove("selected-item");
 
-  // player win
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    return "Win";
-  }
+  computerChoiceImg.src = "images/question-mark.png";
 
-  // tie game
-  if (playerSelection === computerSelection) {
-    return "Tie";
-  }
-};
+  gameOver.innerText = "";
 
-// Play multiple rounds
-const game = function (rounds) {
-  let playerScore = 0;
-  let computerScore = 0;
-  let roundsLeft = rounds;
+  // Play Round
+  actionPlayRound.addEventListener("click", () => {
+    if (mainPage.classList.contains("background--blur")) return;
+    // prompt player to choose if round played without choice
+    if (playerSelect === undefined) {
+      playerNoSelect.classList.remove("hidden");
+      return;
+    }
 
-  // Loop for games.
-  while (roundsLeft > 0) {
-    // Player prompt
-    let playerPlay = prompt("Rock, paper, or scissors? Choose!").toLowerCase();
+    // Computer's Turn
+    computerSelect = computerPlay();
 
-    while (
-      playerPlay !== "rock" &&
-      playerPlay !== "paper" &&
-      playerPlay !== "scissors"
+    if (computerSelect === "rock") {
+      computerChoiceImg.src = "images/rock-clipart.png";
+    }
+    if (computerSelect === "paper") {
+      computerChoiceImg.src = "images/paper-clipart.png";
+    }
+    if (computerSelect === "scissors") {
+      computerChoiceImg.src = "images/scissors-clipart.png";
+    }
+
+    // Player Loses
+    if (
+      (playerSelect === "rock" && computerSelect === "paper") ||
+      (playerSelect === "paper" && computerSelect === "scissors") ||
+      (playerSelect === "scissors" && computerSelect === "rock")
     ) {
-      playerPlay = prompt(
-        "Invalid input. Please select from rock, scissors, or paper."
-      );
+      roundWinner = "comp";
+      computerScoreStored++;
+      nextRoundBtn.innerText = "Computer Wins \n NEXT ROUND";
     }
 
-    // Play one round. Assign it win/lose/tie
-    let oneRound = playRound(playerPlay, computerPlay());
-
-    // How to handle round result
-    if (oneRound === "Win") {
-      playerScore++;
-      roundsLeft--;
+    // Player Wins
+    if (
+      (playerSelect === "rock" && computerSelect === "scissors") ||
+      (playerSelect === "paper" && computerSelect === "rock") ||
+      (playerSelect === "scissors" && computerSelect === "paper")
+    ) {
+      roundWinner = "player";
+      playerScoreStored++;
+      nextRoundBtn.innerText = "Player Wins \n NEXT ROUND";
     }
-    if (oneRound === "Lose") {
-      computerScore++;
-      roundsLeft--;
-    }
-  }
 
-  // Winning conditions
-  return playerScore > computerScore
-    ? `You win! Player: ${playerScore} vs. Computer: ${computerScore}`
-    : `You lose! Computer: ${computerScore} vs Player: ${playerScore}`;
-};
-*/
+    // Tie
+    if (
+      (playerSelect === "rock" && computerSelect === "rock") ||
+      (playerSelect === "paper" && computerSelect === "paper") ||
+      (playerSelect === "scissors" && computerSelect === "scissors")
+    ) {
+      nextRoundBtn.innerText = "Tie: No Points \n NEXT ROUND";
+    }
+
+    // Update Scores
+    displayPlayerScore.innerText = playerScoreStored;
+    displayComputerScore.innerText = computerScoreStored;
+
+    // Winning Conditions
+    //      Next Round
+    if (computerScoreStored !== 5 && playerScoreStored !== 5) {
+      nextRoundBtn.classList.remove("hidden");
+      gameBoard.classList.add("background--blur");
+      mainPage.classList.add("background--blur");
+    }
+
+    //      Player Win
+    if (playerScoreStored === 5) {
+      startGameTitle.innerText = "Restart";
+      startGameText.innerText = "Player Wins!";
+      startGame.classList.remove("hidden");
+      mainPage.classList.add("background--blur");
+      gameBoard.classList.add("background--blur");
+
+      gameOver.innerText = "Player Wins";
+    }
+
+    //      Computer Win
+    if (computerScoreStored === 5) {
+      startGameTitle.innerText = "Restart";
+      startGameText.innerText = "Computer Wins!";
+      startGame.classList.remove("hidden");
+      gameOver.innerText = "Player Loses";
+    }
+  });
+
+  // Start New Round
+  nextRoundBtn.addEventListener("click", () => {
+    nextRound();
+    gameBoard.classList.remove("background--blur");
+    mainPage.classList.remove("background--blur");
+  });
+});
